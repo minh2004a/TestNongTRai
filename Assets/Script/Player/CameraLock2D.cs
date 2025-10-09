7e420147f2745f5cb6a300026ef289f5c4cd42f0
Thanh Minh <116056281+minh2004a@users.noreply.github.com>
2025-10-09 12:52:05 +0700

    Fix Animation AW

---
+++ Assets/Script/Player/CameraLock2D.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CameraLock2D : MonoBehaviour
+{
+    [SerializeField] Transform target;   // kéo Player vào đây
+    [SerializeField] Vector2 offset = Vector2.zero; // lệch khung nếu cần
+
+    void LateUpdate()
+    {
+        if (!target) return;
+        Vector3 p = target.position;
+        transform.position = new Vector3(p.x + offset.x, p.y + offset.y, -10f);
+    }
+}
